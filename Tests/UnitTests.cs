beccf1aaae68c4ae49b197d366820f7d8335b130
Garrett Wilcoxen <garrettwilcoxen@Garretts-Air-63.home.local>
2025-03-26 15:48:16 -0500

    Working .NET 8 build with passing MSTest tests

--- Tests/UnitTests.cs
+++ Tests/UnitTests.cs
@@ -1,12 +1,12 @@
 ﻿using System;
-using NUnit.Framework;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace GithubActionsLab
 {
-    [TestFixture]
-    public class Math
+    [TestClass]
+    public class MathTests
     {
-        [Test]
+        [TestMethod]
         public void Add_Valid()
         {
             Assert.AreEqual(3, Program.Add("1", "2"));
@@ -14,22 +14,22 @@ namespace GithubActionsLab
             Assert.AreEqual(12, Program.Add("5", "7"));
         }
 
-        [Test]
+        [TestMethod]
         public void Add_Invalid()
         {
-            Assert.Throws<FormatException>(() => Program.Add("1", "a"));
-            Assert.Throws<FormatException>(() => Program.Add("a", "1"));
-            Assert.Throws<FormatException>(() => Program.Add("a", "a"));
+            Assert.ThrowsException<FormatException>(() => Program.Add("1", "a"));
+            Assert.ThrowsException<FormatException>(() => Program.Add("a", "1"));
+            Assert.ThrowsException<FormatException>(() => Program.Add("a", "a"));
         }
 
-        [Test]
+        [TestMethod]
         public void Add_Null()
         {
-            Assert.Throws<ArgumentNullException>(() => Program.Add("1", null));
-            Assert.Throws<ArgumentNullException>(() => Program.Add(null, "1"));
-            Assert.Throws<ArgumentNullException>(() => Program.Add(null, null));
+            Assert.ThrowsException<ArgumentNullException>(() => Program.Add("1", null));
+            Assert.ThrowsException<ArgumentNullException>(() => Program.Add(null, "1"));
+            Assert.ThrowsException<ArgumentNullException>(() => Program.Add(null, null));
         }
 
-        // Implement 3 tests per operation, following a similar pattern as above
+        // You can add more [TestMethod]s for Subtract, Multiply, Divide, Power, etc.
     }
 }
